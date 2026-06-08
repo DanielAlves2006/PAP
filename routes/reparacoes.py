ca1a63be141af8d0dee10c94c93c3db30d43e9b3
DanielAlves2006 <simaoalvesdanielalexandre@gmail.com>
2026-06-08 10:54:47 +0100

    modified:   oficina_do_pombo.db 	modified:   routes/__pycache__/reparacoes.cpython-311.pyc 	modified:   routes/reparacoes.py 	modified:   templates/reparacao_admin.html

--- routes/reparacoes.py
+++ routes/reparacoes.py
@@ -1,7 +1,54 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+import sqlite3
 
 reparacoes_route = Blueprint('reparacoes', __name__)
 
 @reparacoes_route.route('/reparacao_admin')
 def reparacoes():
-    return render_template('reparacao_admin.html')
+
+    conn = sqlite3.connect('oficina_do_pombo.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT
+        id,
+        clientes_id,
+        equipamento,
+        marca,
+        modelo,
+        problema,
+        data_reparacao,
+        hora_reparacao,
+        estado
+    FROM reparacoes
+    ORDER BY data_reparacao DESC
+    """)
+
+    reparacoes = cursor.fetchall()
+    
+    conn.close()
+
+    return render_template(
+        'reparacao_admin.html',
+        reparacoes=reparacoes
+    )
+
+
+@reparacoes_route.route('/atualizar_reparacao/<int:id>', methods=['POST'])
+def atualizar_reparacao():
+
+    estado = request.form.get('estado')
+
+    conn = sqlite3.connect('oficina_do_pombo.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE reparacoes
+    SET estado = ?
+    WHERE id = ?
+    """, (estado, id))
+
+    conn.commit()
+    conn.close()
+
+    return redirect('/reparacao_admin')
